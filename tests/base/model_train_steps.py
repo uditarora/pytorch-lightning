f80eae9d73683560870859414d083a68dd99c509
William Falcon <waf2107@columbia.edu>
2020-05-28 09:39:48 -0400

    added train step return

--- tests/base/model_train_steps.py
+++ tests/base/model_train_steps.py
@@ -1,6 +1,7 @@
 import math
 from abc import ABC
 from collections import OrderedDict
+from pytorch_lightning.core.step_result import TrainStepResult
 
 import torch
 
@@ -23,7 +24,7 @@ class TrainingStepVariations(ABC):
         loss_val = self.loss(y, y_hat)
 
         # alternate possible outputs to test
-        if self.trainer.batch_idx % 1 == 0:
+        if batch_idx % 1 == 0:
             output = OrderedDict({
                 'loss': loss_val,
                 'progress_bar': {'some_val': loss_val * loss_val},
@@ -31,7 +32,50 @@ class TrainingStepVariations(ABC):
             })
             return output
 
-        if self.trainer.batch_idx % 2 == 0:
+        if batch_idx % 2 == 0:
+            return loss_val
+
+    def training_step__result_object(self, batch, batch_idx, result):
+        assert isinstance(result, TrainStepResult), 'simple training_step result object not correct class'
+
+        x, y = batch
+        x = x.view(x.size(0), -1)
+
+        y_hat = self(x)
+
+        # calculate loss
+        loss_val = self.loss(y, y_hat)
+
+        # test returning the structured output
+        if batch_idx % 1 == 0:
+            result.minimize = loss_val
+            result.progress_bar_logs = {'some_val': loss_val * loss_val}
+            result.logs = {'train_some_val': loss_val * loss_val}
+            return result
+
+        if batch_idx % 2 == 0:
+            return loss_val
+
+    def training_step__result_object_multiple_optimizers(self, batch, batch_idx, optimizer_idx, result):
+
+        assert isinstance(result, TrainStepResult), 'simple training_step result object not correct class'
+
+        x, y = batch
+        x = x.view(x.size(0), -1)
+
+        y_hat = self(x)
+
+        # calculate loss
+        loss_val = self.loss(y, y_hat)
+
+        # test returning the structured output
+        if optimizer_idx % 1 == 0:
+            result.minimize = loss_val
+            result.progress_bar_logs = {'some_val': loss_val * loss_val}
+            result.logs = {'train_some_val': loss_val * loss_val}
+            return result
+
+        if optimizer_idx % 2 == 0:
             return loss_val
 
     def training_step__inf_loss(self, batch, batch_idx, optimizer_idx=None):
